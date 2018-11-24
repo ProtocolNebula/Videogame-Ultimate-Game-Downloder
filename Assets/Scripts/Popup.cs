15e2eaf72d8dce57eaa375ca22db47a541cba517
ProtocolNebula <powergm@hotmail.es>
2018-11-24 17:46:13 +0100

    Prepared virus controller and popup

---
+++ Assets/Scripts/Popup.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Popup : MonoBehaviour {
+
+    public string text;
+    public GameObject element;
+
+    public Popup(string text)
+    {
+        this.text = text;
+    }
+
+    public Popup(string text, GameObject element)
+    {
+
+    }
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
